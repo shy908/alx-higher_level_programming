6c3feb835d9a2ec2adeb84d5d91568a08a0c0289
shy908 <manuelshylet@gmail.com>
2023-03-08 00:18:10 -0800

    update

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python3 
-import sys 
-sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n') 
+#!/usr/bin/python3
+import sys
+message = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+# write message to stderr
+sys.stderr.write("{}".format(message))
 sys.exit(1)
