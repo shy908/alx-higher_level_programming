c1b5aed4bd5f82c85188a89e953afe0b2ce9c457
shy908 <manuelshylet@gmail.com>
2023-03-08 16:57:49 -0800

    update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-def print_alphabet():
-    alphabet =""
-    for i in range(ord('z'), ord('a') -1, -1):
-        if i % 2 == 1:
-            alphabet += chr(i).upper()
-        else:
-            alphabet += chr(i)
-            print(alphabet, end="")
+for i in range(122, 96, -1):
+    if i % 2 != 0:
+        print('{:c}'.format(i - 32),  end=' ' )
+    else:
+        print('{:c}'.format(i), end=' ')
