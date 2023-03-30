3f8593db1b9926ee9e9bf10ed30db348a7495ea2
shy908 <manuelshylet@gmail.com>
2023-03-29 17:23:53 -0700

    update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-class Square:
-    """Empty class that defines a square"""
+"""Square module.
+This module contains a class that defines a square.
+"""
+
+
+class Square():
+    """Defines a square."""
+
     pass
