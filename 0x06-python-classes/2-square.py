19764e6b5ea4e9a95dc725b5d1c91972a5f61cc9
shy908 <manuelshylet@gmail.com>
2023-03-29 17:45:47 -0700

    update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
-class Square:
-    '''Defines Square'''
+"""Square module.
+This module contains a class that defines a square and init method that
+sets its size and checking if the given values are right.
+"""
+
+
+class Square():
+    """Defines a square."""
+
     def __init__(self, size=0):
-        ''' Initializes data'''
-        self.__size = size
-        ''' checking data inorder to make sure that correct data has been provided'''
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+        """Sets the necessary attributes for the Square object.
+        Args:
+            size (int): the size of one edge of the square.
+        Raises:
+            TypeError: if size is not given as an integer.
+            ValueError: if size is less than 0.
+        """
+        if type(size) is int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer"
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python3
-class Square:
-    """Defines a square"""
-    def __init__(self, size=0):
-        """Initialises the data"""
-        self.size = size
+"""Square module.
+This module contains a class that defines a square and its size and checking
+if the given values are right, and a setter and getter methods to set or get
+it. There's also an area method that returns the area of the square.
+"""
 
-    def area(self):
-        """Returns current square area"""
-        return self.__size**2
+
+class Square():
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Sets the necessary attributes for the Square object.
+        Args:
+            size (int): the size of one edge of the square.
+        """
+        self.size = size
 
     @property
     def size(self):
-        """Getter method"""
+        """Get or set the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Setter method"""
-        self.__size = value
-        if type(value) != int:
+        if type(value) is int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
             raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Returns the current square area."""
+
+        return self.__size ** 2
